a406aae2f7970aef0ede90e4a74e7b7a03e10a83
Steve Haigh <steve.haigh@gmail.com>
2015-01-03 09:25:44 +0000

    Mods to GA

--- Chapter 5/GlobalAlignment.py
+++ Chapter 5/GlobalAlignment.py
@@ -8,32 +8,70 @@ from pprint import pprint
 import sys
 
 
+
+def make_blosum62():
+    rows = """A 4  0 -2 -1 -2  0 -2 -1 -1 -1 -1 -2 -1 -1 -1  1  0  0 -3 -2
+C  0  9 -3 -4 -2 -3 -3 -1 -3 -1 -1 -3 -3 -3 -3 -1 -1 -1 -2 -2
+D -2 -3  6  2 -3 -1 -1 -3 -1 -4 -3  1 -1  0 -2  0 -1 -3 -4 -3
+E -1 -4  2  5 -3 -2  0 -3  1 -3 -2  0 -1  2  0  0 -1 -2 -3 -2
+F -2 -2 -3 -3  6 -3 -1  0 -3  0  0 -3 -4 -3 -3 -2 -2 -1  1  3
+G  0 -3 -1 -2 -3  6 -2 -4 -2 -4 -3  0 -2 -2 -2  0 -2 -3 -2 -3
+H -2 -3 -1  0 -1 -2  8 -3 -1 -3 -2  1 -2  0  0 -1 -2 -3 -2  2
+I -1 -1 -3 -3  0 -4 -3  4 -3  2  1 -3 -3 -3 -3 -2 -1  3 -3 -1
+K -1 -3 -1  1 -3 -2 -1 -3  5 -2 -1  0 -1  1  2  0 -1 -2 -3 -2
+L -1 -1 -4 -3  0 -4 -3  2 -2  4  2 -3 -3 -2 -2 -2 -1  1 -2 -1
+M -1 -1 -3 -2  0 -3 -2  1 -1  2  5 -2 -2  0 -1 -1 -1  1 -1 -1
+N -2 -3  1  0 -3  0  1 -3  0 -3 -2  6 -2  0  0  1  0 -3 -4 -2
+P -1 -3 -1 -1 -4 -2 -2 -3 -1 -3 -2 -2  7 -1 -2 -1 -1 -2 -4 -3
+Q -1 -3  0  2 -3 -2  0 -3  1 -2  0  0 -1  5  1  0 -1 -2 -2 -1
+R -1 -3 -2  0 -3 -2  0 -3  2 -2 -1  0 -2  1  5 -1 -1 -3 -3 -2
+S  1 -1  0  0 -2  0 -1 -2  0 -2 -1  1 -1  0 -1  4  1 -2 -3 -2
+T  0 -1 -1 -1 -2 -2 -2 -1 -1 -1 -1  0 -1 -1 -1  1  5  0 -2 -2
+V  0 -1 -3 -2 -1 -3 -3  3 -2  1  1 -3 -2 -2 -3 -2  0  4 -3 -1
+W -3 -2 -4 -3  1 -2 -2 -3 -3 -2 -1 -4 -4 -2 -3 -3 -2 -3 11  2
+Y -2 -2 -3 -2  3 -3  2 -1 -2 -1 -1 -2 -3 -1 -2 -2 -2 -1  2  7""".split('\n')
+
+    blosum_lookup = {}
+    bases = "A  C  D  E  F  G  H  I  K  L  M  N  P  Q  R  S  T  V  W  Y".split()
+    for row in rows:        
+        scores = row.split()
+        row_dict = {}
+        for i in range(0, len(bases)):
+            row_dict[bases[i]] = int(scores[i+1])
+
+        blosum_lookup[scores[0]] = row_dict
+        
+    return blosum_lookup
+
+
 def lcs_backtrack(v, w):
     """
     Finds the LCS backtrack matrix for two strings, v and w
     """
     print(v,w)
+    indel_penalty = 5
 
     s = [x[:] for x in [[0]*(len(w)+1)]*(len(v)+1)]
     backtrack = [x[:] for x in [['-']*(len(w)+1)]*(len(v)+1)]
+    blosum_lookup_table = make_blosum62()
+
+    ## init the top and side columns with indel penalty
+
 
     for i in range(0, len(v)):
-        for j in range(0, len(w)):
-            if v[i] == w[j]:
-                s[i+1][j+1] = s[i][j] + 1
+        for j in range(0, len(w)):                
+            s[i+1][j+1] = max(s[i][j+1] - indel_penalty, s[i+1][j] - indel_penalty, s[i][j] + blosum_lookup_table[v[i]][w[j]])
+            if s[i+1][j+1] == s[i][j] + blosum_lookup_table[v[i]][w[j]]:
                 backtrack[i+1][j+1] = '#'
-            else:                
-                s[i+1][j+1] = max(s[i][j+1], s[i+1][j])
-
-                if s[i+1][j+1] == s[i][j+1]:
-                    backtrack[i+1][j+1] = 'd'
-                elif s[i+1][j+1] == s[i+1][j]:
-                    backtrack[i+1][j+1] = 'a'
-                else:
-                    backtrack[i+1][j+1] = '!'
+            elif s[i+1][j+1] == s[i][j+1] - indel_penalty:
+                backtrack[i+1][j+1] = 'd'
+            elif s[i+1][j+1] == s[i+1][j] - indel_penalty:
+                backtrack[i+1][j+1] = 'a'
+            else:
+                backtrack[i+1][j+1] = '!'
         
-    pprint(backtrack)
-    pprint(s)
+    ##pprint(backtrack)
+    ##pprint(s)
     return backtrack
 
 
@@ -43,7 +81,7 @@ def output_lcs(backtrack, v, w, i, j):
     result3 = ""
     lcs = ""
 
-    while i > 0 and j > 0:           
+    while i > 0 and j > 0:         
 
         if backtrack[i][j] == 'd':    
             result1 = v[i-1] + result1      
@@ -88,8 +126,8 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    v = "TGTACG"
-    w = "GCTAGT"
+    w = "ACDF"
+    v = "CDFF"
 
     b = lcs_backtrack(v, w)
 
